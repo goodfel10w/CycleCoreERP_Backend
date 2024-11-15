2aa05cea2f49a764d22820084da2eaba7708d6bc
goodFell0w <denislischer@gmail.com>
2024-11-15 21:52:59 +0100

    Initial commit

---
+++ CycleCoreERP_Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CycleCoreERP_Backend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
