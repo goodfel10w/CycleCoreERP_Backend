b1ee85f02405a954737cbd73aad30a701eb9ca5b
Fubi <hqfubi@gmail.com>
2024-11-18 19:17:24 +0100

    DispositionsRechnung erweitert

--- CycleCoreERP_Backend/ProzessKalkulation/Disposition.cs
+++ CycleCoreERP_Backend/ProzessKalkulation/Disposition.cs
@@ -30,35 +30,61 @@ namespace CycleCoreERP_Backend.ProzessKalkulation
 
         ==> Fertigungsauftrag für das Bauteil (int)
          */
-        private int _zuProudzierendenFahraeder = 150; // Placeholder für den Userinput
-        private int _sicherheitsLagerbestand = 50; // Placeholder für den Userinput
-        public static int BerechneFertigungsauftragsVolumenFürBauteil(Bauteil bauteil)
+        /*        private int _zuProudzierendenFahraeder = 150; // Placeholder für den Userinput
+                private int _sicherheitsLagerbestand = 50; // Placeholder für den Userinput
+                public static int BerechneFertigungsauftragsVolumenFürBauteil(Bauteil bauteil)
+                {
+                    int warteschlangeFahrrad = XMLService.GetAnzahlArtikelInWarteschlangen(bauteil.VerbautInTeil);
+                    int anzInWarteschlange = XMLService.GetAnzahlArtikelInWarteschlangen(bauteil.Id);
+                    int anzahlArtikelInBearbeitung = XMLService.GetAnzahlArtikelInBearbeitung(bauteil.Id);
+
+                    int lagerbestand = XMLService.GetArtikelBestand(bauteil.Id);
+
+                    int ergebnis = 0;
+
+                    return 0;
+                }
+        */
+        public void BerechnungFahrraederAktuellePeriode(List<List<int>> prognose)
         {
-            int warteschlangeFahrrad = XMLService.GetAnzahlArtikelInWarteschlangen(bauteil.VerbautInTeil);
-            int anzInWarteschlange = XMLService.GetAnzahlArtikelInWarteschlangen(bauteil.Id);
-            int anzahlArtikelInBearbeitung = XMLService.GetAnzahlArtikelInBearbeitung(bauteil.Id);
+            _prognose = prognose;
 
-            int lagerbestand = XMLService.GetArtikelBestand(bauteil.Id);
-
-            int ergebnis = 0;
-
-            return 0;
-        }
-
-        public void BerechnungFahrraederAktuellePeriode()
-        {
             foreach (Fahrrad fahrrad in Enum.GetValues(typeof(Fahrrad)))
             {
                 var stuekliste = Data.Stueckliste.Bauteile[fahrrad];
+                var sicherheitsBestand = 0;
 
                 foreach (var bauteil in stuekliste)
                 {
+                    var lagerBestand = XMLService.GetArtikelBestand(bauteil.Id);
+                    var anzahlInWarteschlange = XMLService.GetAnzahlArtikelInWarteschlangen(bauteil.Id);
+                    var anzahlInBearbeitung = XMLService.GetAnzahlArtikelInBearbeitung(bauteil.Id);
 
+                    int produktionsMenge;
+                    if (bauteil.Id == (int)fahrrad)
+                    {
+                        int anzahlFahrraederProduzieren = GetPrognose(periode: 0, bauteil: (int)fahrrad); // Prognose verstehen
+                        produktionsMenge = anzahlFahrraederProduzieren + sicherheitsBestand - lagerBestand - anzahlInWarteschlange - anzahlInBearbeitung;
+                    }
+                    else
+                    {
+                        produktionsMenge = sicherheitsBestand - lagerBestand - anzahlInWarteschlange - anzahlInBearbeitung;
+                    }
                 }
 
             }
 
 
+        }
+
+        private int GetPrognose(int periode, int bauteil)
+        {
+            if (periode == 10)
+            {
+                var insgesamt = _prognose[1][bauteil] + _prognose[2][bauteil] + _prognose[3][bauteil];
+                return (int)(insgesamt / 3);
+            }
+            return _prognose[periode][bauteil];
         }
 
     }
